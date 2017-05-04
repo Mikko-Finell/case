c1a278638a9dff717ce966c0b5301dbe0e8319dd
Mikko Finell <mikko.finell@gmail.com>
2017-05-04 07:14:12 +0300

    update tests

--- test/t_update.hpp
+++ test/t_update.hpp
@@ -1,6 +1,6 @@
 #include <algorithm>
 #include <cpptest.hpp>
-#include "../updatemanager.hpp"
+#include "../update.hpp"
 
 namespace t_update {
 
@@ -15,19 +15,36 @@ public:
 };
 
 bool serial_01() {
+    constexpr auto SIZE = 100;
     CASE::update::Serial<Agent> upd;
-#define SIZE 100
     Agent current[SIZE];
     Agent next[SIZE];
     upd.launch(current, next, SIZE);
-#undef SIZE
+    return std::all_of(std::begin(next), std::end(next),
+        [](const Agent & a){ return a.updated; });
+}
+
+bool serial_02() {
+    constexpr auto SIZE = 100;
+    CASE::update::Serial<Agent> upd;
+    Agent current[SIZE];
+    Agent next[SIZE];
+
+    const auto generations = 1000;
+    for (auto i = 0; i < generations; i++) {
+        for (auto & agent : next)
+            agent.updated = false;
+
+        upd.launch(current, next, SIZE);
+        upd.wait();
+    }
     return std::all_of(std::begin(next), std::end(next),
         [](const Agent & a){ return a.updated; });
 }
 
 bool parallel_01() {
+    constexpr auto SIZE = 100;
     CASE::update::Parallel<Agent> upd;
-#define SIZE 100
     Agent current[SIZE];
     Agent next[SIZE];
     upd.init();
@@ -35,7 +52,26 @@ bool parallel_01() {
     upd.launch(current, next, SIZE);
     upd.wait();
     upd.terminate();
-#undef SIZE
+    return std::all_of(std::begin(next), std::end(next),
+        [](const Agent & a){ return a.updated; });
+}
+
+bool parallel_02() {
+    constexpr auto SIZE = 100;
+    CASE::update::Parallel<Agent> upd;
+    Agent current[SIZE];
+    Agent next[SIZE];
+    upd.init();
+
+    const auto generations = 1000;
+    for (auto i = 0; i < generations; i++) {
+        for (auto & agent : next)
+            agent.updated = false;
+
+        upd.launch(current, next, SIZE);
+        upd.wait();
+    }
+    upd.terminate();
     return std::all_of(std::begin(next), std::end(next),
         [](const Agent & a){ return a.updated; });
 }
@@ -43,6 +79,8 @@ bool parallel_01() {
 void run() {
     cpptest::Module test{"update"};
     test.fn("serial update 01", serial_01);
+    test.fn("serial update 02", serial_02);
     test.fn("parallel update 01", parallel_01);
+    test.fn("parallel update 02", parallel_02);
 }
 }
