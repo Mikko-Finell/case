ddc548eaa7557be0aae9471f1c8c38ee643109a4
Mikko Finell <mikko.finell@gmail.com>
2017-05-20 07:43:44 +0300

    add methods try_move and try_spawn; also add temporary sorting implmentation

--- world.hpp
+++ world.hpp
@@ -1,71 +1,145 @@
 #ifndef CASE_WORLD
 #define CASE_WORLD
 
-#include <algorithm>
-#include <functional>
-#include <vector>
+#include <cassert>
+#include <deque>
 
+#include "code.hpp"
 #include "gc.hpp"
-#include "grid.hpp"
 
 namespace CASE {
 
-template<class Cell>
+template<class Agent, class Grid>
 class World {
-    using Agent = typename Cell::Agent;
-
     const int max_agents;
-    int live_count = 0;
+    std::deque<int> available_indices;
     GarbageCollector<Agent> gc;
+
+    class _mover {
+        World<Agent, Grid> & world;
+        Agent & agent;
+
+    public:
+        _mover(World<Agent, Grid> & w, Agent & a) : world(w), agent(a)
+        {}
+
+        Code to(const int x, const int y) {
+            return world.try_move(agent, x, y);
+        }
+    };
 
 public:
     Agent * agents;
-    Grid<Cell> & grid;
+    Grid grid;
 
-    World(const int array_size, Grid<Cell> & _grid)
-        : max_agents(array_size), gc{array_size}, grid(_grid)
+    World(const int array_size)
+        : max_agents(array_size)
+        , gc{max_agents}
     {
         agents = new Agent[max_agents];
-        for (auto i = 0; i < max_agents; i++)
+        for (auto i = 0; i < max_agents; i++) {
+            available_indices.push_back(i);
             agents[i].deactivate();
+        }
     }
 
     ~World() {
         delete [] agents;
     }
 
-    int count() const {
-        int n = 0;
-        for (auto i = 0; i < max_agents; i++) {
-            if (agents[i].active())
-                ++n;
-        }
-        return n;
+    int count() {
+        //std::cout << "MAX " << max_agents << std::endl;
+        //std::cout << "FREE " << available_indices.size() << std::endl;
+        //std::cout << "LIVE " << max_agents - available_indices.size() << std::endl;
+        gc.compact(agents);
+        return max_agents - available_indices.size();
     }
 
-    Agent * spawn() {
-        auto live_count = count();
-        if (live_count == max_agents)
-            return nullptr;
+    Code try_move(Agent & agent, const int x, const int y) {
+        assert(agent.cell != nullptr);
 
-        return &agents[live_count];
+        auto cell = agent.cell;
+        cell->extract(agent);
+
+        if (grid(x, y).insert(agent) == Code::OK) {
+            agent.x = wrap(x, grid.columns);
+            agent.y = wrap(y, grid.rows);
+            return Code::OK;
+        }
+        else if (cell->insert(agent) == Code::OK)
+            return Code::Rejected;
+        else
+            return Code::InternalError;
+    }
+
+    auto try_move(Agent & agent) {
+        return _mover{*this, agent};
+    }
+
+    Agent * try_spawn(const Agent & agent) {
+        assert(available_indices.empty() == false);
+        if (available_indices.empty()) {
+            std::cout << "Cant spawn because no available slots left" << std::endl;
+            return nullptr;
+        }
+
+        const auto index = available_indices.front();
+
+        // copy the argument into the array slot at index
+        agents[index] = agent;
+        auto ptr = &agents[index];
+
+        ptr->activate();
+        const auto code = grid(agent.x, agent.y).insert(ptr);
+        if (code == Code::OK) {
+            available_indices.pop_front();
+            return ptr;
+        }
+        else {
+            //std::cout << "Cant spawn because was unable to insert" << std::endl;
+            ptr->deactivate();
+            return nullptr;
+        }
     }
 
     Code kill(Agent & agent) {
-        if (agent.cell != nullptr)
-            agent.cell->extract(agent);
+        for (auto i = 0; i < max_agents; i++) {
+            if (&agent == &agents[i]) {
+                agent.cell->extract(agent.z);
+                assert(agent.cell == nullptr);
+                agent.deactivate();
+                available_indices.push_back(i);
+                return Code::OK;
+            }
+        }
+        return Code::NotFound;
+    }
 
-        agent.deactivate();
-        assert(agent.cell == nullptr);
-
-        live_count = gc.compact(agents);
-        return Code::OK;
+    void sort() {
+        gc.compact(agents);
+        available_indices.clear();
+        bool expect_dead = false;
+        for (auto i = 0; i < max_agents; i++) {
+            if (agents[i].active() == false) {
+                available_indices.push_back(i);
+                expect_dead = true;
+            }
+            else {
+                if (expect_dead) {
+                    assert(true == false);
+                }
+            }
+        }
     }
 
     void clear() {
         grid.clear();
-        for (auto i = 0; i < max_agents; i++)
+        available_indices.clear();
+        for (auto i = 0; i < max_agents; i++) {
             agents[i] = Agent{};
+            agents[i].deactivate();
+            available_indices.push_back(i);
+        }
     }
 };
 
