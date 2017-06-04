c2bd3070919b042119773755bdbce29cddc10256
Mikko Finell <mikko.finell@gmail.com>
2017-06-04 16:54:16 +0300

    update test

--- test/t_random.cpp
+++ test/t_random.cpp
@@ -1,37 +1,13 @@
 #include <iostream>
 #include <array>
+
+#define CASE_DETERMINISTIC
 #include "../random.hpp"
 
 int main() {
     using namespace std;
     using namespace CASE;
 
-    constexpr int range = 255;
-    array<int, range > results;
-
-    for (auto i = 0; i < range; i++) {
-        results[i] = 0;
-    }
-
-    Cauchy rand;
-
-    for (auto i = 0; i < range * 20; i++) {
-        const auto index = rand(255/2, 5);
-        if (index >= 0 && index < range)
-            results[index] += 1;
-    }
-
-    for (auto i = 0; i < results.size(); i++) {
-        cout << i << " ";
-        if (i < 100)
-            cout << " ";
-        if (i < 10)
-            cout << " ";
-        cout << "|";
-
-        const auto xs = results.at(i);
-        for (auto i = 0; i < xs; i += 5)
-            cout << "*";
-        cout << endl;
-    }
+    for (int i = 0; i < 40; i++)
+        cout << seed() << endl;
 }
