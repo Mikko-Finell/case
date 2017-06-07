143c6650b2b0ff17d816cbd3b0c27b2358a08ec0
Mikko Finell <mikko.finell@gmail.com>
2017-06-07 07:08:19 +0300

    add comments and simplify logic

--- index.hpp
+++ index.hpp
@@ -8,23 +8,24 @@
 
 namespace CASE {
 
+// n mod size with floored division, so as to wrap backwards around negative n
 inline int wrap(const int n, const int size) {
     assert(size > 0);
-    if (n >= 0)
-        return n % size;
-    else
-        return ((n % size) + size) % size;
+    return ((n % size) + size) % size;
 }
 
+// row-major major matrix index 
 inline int index(const int x, const int y, const int size) {
     assert(size > 0);
     return y * size + x;
 }
 
+namespace _impl {
 template<class T>
 inline T & bda(T * t, int i, int c, int r, int x, int y) {
     return *(t+(((i%c+x)%c)+c)%c+((((i/c+y)%r)+r)%r)*c-i);
 }
+} // __impl
 
 } // CASE
 
