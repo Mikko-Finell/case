b8f74a05fd2925b0a5f6297b76ee9911b26bb164
Mikko Finell <mikko.finell@gmail.com>
2017-06-08 10:35:38 +0300

    add inline to functions

--- quad.hpp
+++ quad.hpp
@@ -8,7 +8,7 @@
 
 namespace CASE {
 
-namespace impl {
+namespace _impl {
 inline void setpos(sf::Vertex & vs, const int x, const int y) {
     vs.position.x = x;
     vs.position.y = y;
@@ -19,13 +19,13 @@ inline void setcolor(sf::Vertex & vs, const int r, const int g, const int b) {
     vs.color.g = g;
     vs.color.b = b;
 }
-} // impl
+} // _impl
 
 inline
 void quad(const int x, const int y, const int w, const int h,
           const int r, const int g, const int b, sf::Vertex * vs)
 {
-    using namespace impl;
+    using namespace _impl;
     setpos(vs[0], x, y);
     setpos(vs[1], x + w, y);
     setpos(vs[2], x + w, y + h);
@@ -38,42 +38,41 @@ void quad(const int x, const int y, const int w, const int h,
 
 inline
 void quad(const int x, const int y, const int w, const int h, sf::Vertex * vs) {
-    using namespace impl;
+    using namespace _impl;
     setpos(vs[0], x, y);
     setpos(vs[1], x + w, y);
     setpos(vs[2], x + w, y + h);
     setpos(vs[3], x, y + h);
 }
 
-inline
-void quad(const int r, const int g, const int b, sf::Vertex * vs) {
-    using namespace impl;
+inline void quad(const int r, const int g, const int b, sf::Vertex * vs) {
+    using namespace _impl;
     setcolor(vs[0], r, g, b);
     setcolor(vs[1], r, g, b);
     setcolor(vs[2], r, g, b);
     setcolor(vs[3], r, g, b);
 }
 
-template<class Position, class Size, class Color>
-void quad(const Position & pos, const Size & size, const Color & color,
-          sf::Vertex * vs)
+template <class Position, class Size, class Color>
+inline void quad(const Position & pos, const Size & size, const Color & color,
+                 sf::Vertex * vs)
 {
     vs[0] = sf::Vertex{pos, color};
-    vs[1] = sf::Vertex{pos + sf::Vector2f{size.x,0}, color};
+    vs[1] = sf::Vertex{pos + sf::Vector2f{size.x, 0}, color};
     vs[2] = sf::Vertex{pos + size, color};
     vs[3] = sf::Vertex{pos + sf::Vector2f{0, size.y}, color};
 }
 
-template<class Position, class Size>
-void quad(const Position & pos, const Size & size, sf::Vertex * vs) {
+template <class Position, class Size>
+inline void quad(const Position & pos, const Size & size, sf::Vertex * vs) {
     vs[0].position = pos;
-    vs[1].position = pos + sf::Vector2f{size.x,0};
+    vs[1].position = pos + sf::Vector2f{size.x, 0};
     vs[2].position = pos + size;
     vs[3].position = pos + sf::Vector2f{0, size.y};
 }
 
-template<class Color>
-void quad(const Color & color, sf::Vertex * vs) {
+template <class Color>
+inline void quad(const Color & color, sf::Vertex * vs) {
     vs[0].color = color;
     vs[1].color = color;
     vs[2].color = color;
